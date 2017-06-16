41c9c9bd660cb366135d13679013d82a154173c5
eead-csic-compbio <compbio@eead.csic.es>
2017-06-16 15:40:38 +0200

    First full html content

---
+++ images.pl
@@ -0,0 +1,78 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{DBIflux}{{center{{figure};LFS=12;FSF=1/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="547" HEIGHT="496" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="\begin{figure}
+\begin{center}
+\includegraphics[width=0.8\textwidth]{DBIflux}
+\end{center}
+\end{figure}">|; 
+
+$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{1lfu}{{center{{figure};LFS=12;FSF=1/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="328" HEIGHT="387" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\begin{figure}
+\begin{center}
+\includegraphics[width=0.8\textwidth]{1lfu}
+\end{center}
+\end{figure}">|; 
+
+$key = q/Ngeq1;MSF=1.4;LFS=12/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="44" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$ N\geq1$">|; 
+
+$key = q/includegraphics[width=0.8textwidth]{subrutinas};LFS=12/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="390" HEIGHT="496" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\includegraphics[width=0.8\textwidth]{subrutinas}">|; 
+
+$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{colaypila}{{center{{figure};LFS=12;FSF=1/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="547" HEIGHT="357" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="\begin{figure}
+\begin{center}
+\includegraphics[width=0.8\textwidth]{colaypila}
+\end{center}
+\end{figure}">|; 
+
+$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{clase}{{center{{figure};LFS=12;FSF=1/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="416" HEIGHT="295" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="\begin{figure}
+\begin{center}
+\includegraphics[width=0.8\textwidth]{clase}
+\end{center}
+\end{figure}">|; 
+
+$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{BDoperaciones}{{center{{figure};LFS=12;FSF=1/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="547" HEIGHT="435" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="\begin{figure}
+\begin{center}
+\includegraphics[width=0.8\textwidth]{BDoperaciones}
+\end{center}
+\end{figure}">|; 
+
+$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{arbolBin}{{center{{figure};LFS=12;FSF=1/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="547" HEIGHT="361" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="\begin{figure}
+\begin{center}
+\includegraphics[width=0.8\textwidth]{arbolBin}
+\end{center}
+\end{figure}">|; 
+
+1;
+
---
+++ internals.pl
@@ -0,0 +1,218 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_SWISS-PROT2003/;
+$ref_files{$key} = "$dir".q|node100.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/apoyo/;
+$ref_files{$key} = "$dir".q|node92.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Berman2000/;
+$ref_files{$key} = "$dir".q|node100.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Gutierrez-Rios2004/;
+$ref_files{$key} = "$dir".q|node102.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/bioperl/;
+$ref_files{$key} = "$dir".q|node86.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/problMatrSust/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/problSO/;
+$ref_files{$key} = "$dir".q|node40.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/GDGraph/;
+$ref_files{$key} = "$dir".q|node73.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_GenBank2002/;
+$ref_files{$key} = "$dir".q|node100.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/funcArreglos/;
+$ref_files{$key} = "$dir".q|node23.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:arbolBin/;
+$ref_files{$key} = "$dir".q|node56.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/problEnzRest/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Henikoff1993/;
+$ref_files{$key} = "$dir".q|node100.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Thompson1994/;
+$ref_files{$key} = "$dir".q|node100.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/SOAP/;
+$ref_files{$key} = "$dir".q|node75.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/RSPerl/;
+$ref_files{$key} = "$dir".q|node74.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/varEspecial/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/CPAN/;
+$ref_files{$key} = "$dir".q|node64.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:colaypila/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:BDoperaciones/;
+$ref_files{$key} = "$dir".q|node80.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/registros/;
+$ref_files{$key} = "$dir".q|node54.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/asigArreglos/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sample_tree/;
+$ref_files{$key} = "$dir".q|node86.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sem2/;
+$ref_files{$key} = "$dir".q|node41.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:DBIflux/;
+$ref_files{$key} = "$dir".q|node79.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sample_map/;
+$ref_files{$key} = "$dir".q|node85.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ventajSub/;
+$ref_files{$key} = "$dir".q|node48.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Muller2004/;
+$ref_files{$key} = "$dir".q|node100.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/pasoref/;
+$ref_files{$key} = "$dir".q|node46.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/problEvalAlin/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/aritfunc/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sem1/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/variab_tipos/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/terminal/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/editores/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/presentacion/;
+$ref_files{$key} = "$dir".q|node1.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/perl6/;
+$ref_files{$key} = "$dir".q|node87.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/problBLAST/;
+$ref_files{$key} = "$dir".q|node36.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sem4/;
+$ref_files{$key} = "$dir".q|node89.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graphviz/;
+$ref_files{$key} = "$dir".q|node74.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/bibFunciones/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:subrutinas/;
+$ref_files{$key} = "$dir".q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/stringfunc/;
+$ref_files{$key} = "$dir".q|node24.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/oneliners/;
+$ref_files{$key} = "$dir".q|node89.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cicloD/;
+$ref_files{$key} = "$dir".q|node89.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/clase/;
+$ref_files{$key} = "$dir".q|node61.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:interfaz_graphviz/;
+$ref_files{$key} = "$dir".q|node74.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:clase/;
+$ref_files{$key} = "$dir".q|node58.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sem3/;
+$ref_files{$key} = "$dir".q|node78.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:1lfu/;
+$ref_files{$key} = "$dir".q|node35.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Altschul1997/;
+$ref_files{$key} = "$dir".q|node100.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sem5/;
+$ref_files{$key} = "$dir".q|node96.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ap/;
+$ref_files{$key} = "$dir".q|node100.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
