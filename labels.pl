1bf9746ac873c1a4d74fcbb80233f65ef7025295
eead-csic-compbio <compbio@eead.csic.es>
2017-06-19 09:57:07 +0200

    Added HTTP::Server::Brick oneliner and cpanm

--- labels.pl
+++ labels.pl
@@ -2,71 +2,107 @@
 # Associate labels original text with physical files.
 
 
-$key = q/apoyo/;
-$external_labels{$key} = "$URL/" . q|node91.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_GenBank2002/;
-$external_labels{$key} = "$URL/" . q|node100.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/variab_tipos/;
-$external_labels{$key} = "$URL/" . q|node9.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/problMatrSust/;
-$external_labels{$key} = "$URL/" . q|node28.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_Thompson1994/;
-$external_labels{$key} = "$URL/" . q|node100.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/editores/;
-$external_labels{$key} = "$URL/" . q|node5.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/problEvalAlin/;
-$external_labels{$key} = "$URL/" . q|node29.html|; 
+$key = q/fig:sample_tree/;
+$external_labels{$key} = "$URL/" . q|node86.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sem5/;
 $external_labels{$key} = "$URL/" . q|node96.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_Henikoff1993/;
-$external_labels{$key} = "$URL/" . q|node100.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/aritfunc/;
-$external_labels{$key} = "$URL/" . q|node26.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/RSPerl/;
-$external_labels{$key} = "$URL/" . q|node74.html|; 
+$key = q/CPAN/;
+$external_labels{$key} = "$URL/" . q|node63.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/GDGraph/;
 $external_labels{$key} = "$URL/" . q|node72.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/terminal/;
-$external_labels{$key} = "$URL/" . q|node4.html|; 
+$key = q/cite_Muller2004/;
+$external_labels{$key} = "$URL/" . q|node100.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/bioperl/;
-$external_labels{$key} = "$URL/" . q|node85.html|; 
+$key = q/fig:DBIflux/;
+$external_labels{$key} = "$URL/" . q|node78.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:BDoperaciones/;
+$external_labels{$key} = "$URL/" . q|node79.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/problEnzRest/;
+$external_labels{$key} = "$URL/" . q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Berman2000/;
+$external_labels{$key} = "$URL/" . q|node100.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sem1/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:subrutinas/;
 $external_labels{$key} = "$URL/" . q|node41.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/problSO/;
-$external_labels{$key} = "$URL/" . q|node40.html|; 
+$key = q/ap/;
+$external_labels{$key} = "$URL/" . q|node100.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_Muller2004/;
+$key = q/ventajSub/;
+$external_labels{$key} = "$URL/" . q|node47.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/pasoref/;
+$external_labels{$key} = "$URL/" . q|node45.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:arbolBin/;
+$external_labels{$key} = "$URL/" . q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/SOAP/;
+$external_labels{$key} = "$URL/" . q|node74.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cicloD/;
+$external_labels{$key} = "$URL/" . q|node89.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/funcArreglos/;
+$external_labels{$key} = "$URL/" . q|node23.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Thompson1994/;
+$external_labels{$key} = "$URL/" . q|node100.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:colaypila/;
+$external_labels{$key} = "$URL/" . q|node54.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/perl6/;
+$external_labels{$key} = "$URL/" . q|node86.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Henikoff1993/;
+$external_labels{$key} = "$URL/" . q|node100.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/registros/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Altschul1997/;
+$external_labels{$key} = "$URL/" . q|node100.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/asigArreglos/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_GenBank2002/;
 $external_labels{$key} = "$URL/" . q|node100.html|; 
 $noresave{$key} = "$nosave";
 
@@ -74,63 +110,31 @@ $key = q/clase/;
 $external_labels{$key} = "$URL/" . q|node60.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_Altschul1997/;
-$external_labels{$key} = "$URL/" . q|node100.html|; 
+$key = q/problBLAST/;
+$external_labels{$key} = "$URL/" . q|node36.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/pasoref/;
-$external_labels{$key} = "$URL/" . q|node45.html|; 
+$key = q/stringfunc/;
+$external_labels{$key} = "$URL/" . q|node24.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:colaypila/;
-$external_labels{$key} = "$URL/" . q|node54.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/presentacion/;
-$external_labels{$key} = "$URL/" . q|node1.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/bibFunciones/;
-$external_labels{$key} = "$URL/" . q|node22.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sem2/;
-$external_labels{$key} = "$URL/" . q|node40.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:BDoperaciones/;
-$external_labels{$key} = "$URL/" . q|node79.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sem1/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
+$key = q/oneliners/;
+$external_labels{$key} = "$URL/" . q|node88.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:sample_map/;
 $external_labels{$key} = "$URL/" . q|node85.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/problBLAST/;
-$external_labels{$key} = "$URL/" . q|node36.html|; 
+$key = q/editores/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/SOAP/;
-$external_labels{$key} = "$URL/" . q|node74.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/problEnzRest/;
-$external_labels{$key} = "$URL/" . q|node21.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:cicloD/;
-$external_labels{$key} = "$URL/" . q|node89.html|; 
+$key = q/problSO/;
+$external_labels{$key} = "$URL/" . q|node40.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:interfaz_graphviz/;
-$external_labels{$key} = "$URL/" . q|node73.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/graphviz/;
 $external_labels{$key} = "$URL/" . q|node73.html|; 
 $noresave{$key} = "$nosave";
 
@@ -138,80 +142,76 @@ $key = q/cite_Gutierrez-Rios2004/;
 $external_labels{$key} = "$URL/" . q|node102.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_Berman2000/;
+$key = q/cite_SWISS-PROT2003/;
 $external_labels{$key} = "$URL/" . q|node100.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/perl6/;
-$external_labels{$key} = "$URL/" . q|node86.html|; 
+$key = q/terminal/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/stringfunc/;
-$external_labels{$key} = "$URL/" . q|node24.html|; 
+$key = q/sem2/;
+$external_labels{$key} = "$URL/" . q|node40.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/registros/;
-$external_labels{$key} = "$URL/" . q|node53.html|; 
+$key = q/aritfunc/;
+$external_labels{$key} = "$URL/" . q|node26.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/oneliners/;
-$external_labels{$key} = "$URL/" . q|node88.html|; 
+$key = q/bioperl/;
+$external_labels{$key} = "$URL/" . q|node85.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/varEspecial/;
-$external_labels{$key} = "$URL/" . q|node13.html|; 
+$key = q/graphviz/;
+$external_labels{$key} = "$URL/" . q|node73.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:sample_tree/;
-$external_labels{$key} = "$URL/" . q|node86.html|; 
+$key = q/presentacion/;
+$external_labels{$key} = "$URL/" . q|node1.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/ventajSub/;
-$external_labels{$key} = "$URL/" . q|node47.html|; 
+$key = q/variab_tipos/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/apoyo/;
+$external_labels{$key} = "$URL/" . q|node91.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:clase/;
 $external_labels{$key} = "$URL/" . q|node57.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/sem3/;
-$external_labels{$key} = "$URL/" . q|node77.html|; 
+$key = q/sem4/;
+$external_labels{$key} = "$URL/" . q|node89.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/CPAN/;
-$external_labels{$key} = "$URL/" . q|node63.html|; 
+$key = q/problMatrSust/;
+$external_labels{$key} = "$URL/" . q|node28.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:1lfu/;
 $external_labels{$key} = "$URL/" . q|node35.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:DBIflux/;
-$external_labels{$key} = "$URL/" . q|node78.html|; 
+$key = q/varEspecial/;
+$external_labels{$key} = "$URL/" . q|node13.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:arbolBin/;
-$external_labels{$key} = "$URL/" . q|node55.html|; 
+$key = q/problEvalAlin/;
+$external_labels{$key} = "$URL/" . q|node29.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_SWISS-PROT2003/;
-$external_labels{$key} = "$URL/" . q|node100.html|; 
+$key = q/bibFunciones/;
+$external_labels{$key} = "$URL/" . q|node22.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/sem4/;
-$external_labels{$key} = "$URL/" . q|node89.html|; 
+$key = q/RSPerl/;
+$external_labels{$key} = "$URL/" . q|node74.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/asigArreglos/;
-$external_labels{$key} = "$URL/" . q|node11.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ap/;
-$external_labels{$key} = "$URL/" . q|node100.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/funcArreglos/;
-$external_labels{$key} = "$URL/" . q|node23.html|; 
+$key = q/sem3/;
+$external_labels{$key} = "$URL/" . q|node77.html|; 
 $noresave{$key} = "$nosave";
 
 1;
@@ -221,152 +221,152 @@ $noresave{$key} = "$nosave";
 # labels from external_latex_labels array.
 
 
-$key = q/bibFunciones/;
-$external_latex_labels{$key} = q|2.7|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sem2/;
-$external_latex_labels{$key} = q|3|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:BDoperaciones/;
-$external_latex_labels{$key} = q|4.2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sem1/;
-$external_latex_labels{$key} = q|2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/clase/;
-$external_latex_labels{$key} = q|3.3.3|; 
-$noresave{$key} = "$nosave";
-
-$key = q/pasoref/;
-$external_latex_labels{$key} = q|3.1.3|; 
-$noresave{$key} = "$nosave";
-
-$key = q/presentacion/;
-$external_latex_labels{$key} = q|1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:colaypila/;
-$external_latex_labels{$key} = q|3.2|; 
+$key = q/CPAN/;
+$external_latex_labels{$key} = q|3.4.2|; 
 $noresave{$key} = "$nosave";
 
 $key = q/GDGraph/;
 $external_latex_labels{$key} = q|3.4.6|; 
 $noresave{$key} = "$nosave";
 
-$key = q/bioperl/;
-$external_latex_labels{$key} = q|4.2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/terminal/;
-$external_latex_labels{$key} = q|2.1.1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:subrutinas/;
-$external_latex_labels{$key} = q|3.1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/aritfunc/;
-$external_latex_labels{$key} = q|2.7.4|; 
-$noresave{$key} = "$nosave";
-
-$key = q/editores/;
-$external_latex_labels{$key} = q|2.1.2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/problEvalAlin/;
-$external_latex_labels{$key} = q|2.7.5|; 
-$noresave{$key} = "$nosave";
-
-$key = q/variab_tipos/;
-$external_latex_labels{$key} = q|2.3|; 
-$noresave{$key} = "$nosave";
-
-$key = q/problMatrSust/;
-$external_latex_labels{$key} = q|2.7.5|; 
-$noresave{$key} = "$nosave";
-
-$key = q/apoyo/;
-$external_latex_labels{$key} = q|10|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:arbolBin/;
-$external_latex_labels{$key} = q|3.3|; 
-$noresave{$key} = "$nosave";
-
-$key = q/asigArreglos/;
-$external_latex_labels{$key} = q|2.3.2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/funcArreglos/;
-$external_latex_labels{$key} = q|2.7.1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ventajSub/;
-$external_latex_labels{$key} = q|3.1.5|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:clase/;
-$external_latex_labels{$key} = q|3.4|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sem3/;
-$external_latex_labels{$key} = q|4|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:1lfu/;
-$external_latex_labels{$key} = q|2.1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/CPAN/;
-$external_latex_labels{$key} = q|3.4.2|; 
-$noresave{$key} = "$nosave";
-
 $key = q/fig:DBIflux/;
 $external_latex_labels{$key} = q|4.1|; 
 $noresave{$key} = "$nosave";
 
-$key = q/registros/;
-$external_latex_labels{$key} = q|3.2.4|; 
-$noresave{$key} = "$nosave";
-
-$key = q/stringfunc/;
-$external_latex_labels{$key} = q|2.7.2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/oneliners/;
-$external_latex_labels{$key} = q|6|; 
-$noresave{$key} = "$nosave";
-
-$key = q/varEspecial/;
-$external_latex_labels{$key} = q|2.3.4|; 
-$noresave{$key} = "$nosave";
-
-$key = q/perl6/;
-$external_latex_labels{$key} = q|5|; 
-$noresave{$key} = "$nosave";
-
-$key = q/graphviz/;
-$external_latex_labels{$key} = q|3.4.6|; 
-$noresave{$key} = "$nosave";
-
-$key = q/problBLAST/;
-$external_latex_labels{$key} = q|2.8.3|; 
-$noresave{$key} = "$nosave";
-
-$key = q/SOAP/;
-$external_latex_labels{$key} = q|3.4.6|; 
+$key = q/fig:BDoperaciones/;
+$external_latex_labels{$key} = q|4.2|; 
 $noresave{$key} = "$nosave";
 
 $key = q/problEnzRest/;
 $external_latex_labels{$key} = q|2.6.1|; 
 $noresave{$key} = "$nosave";
 
+$key = q/sem1/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:subrutinas/;
+$external_latex_labels{$key} = q|3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/pasoref/;
+$external_latex_labels{$key} = q|3.1.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ventajSub/;
+$external_latex_labels{$key} = q|3.1.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/SOAP/;
+$external_latex_labels{$key} = q|3.4.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:arbolBin/;
+$external_latex_labels{$key} = q|3.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/funcArreglos/;
+$external_latex_labels{$key} = q|2.7.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:colaypila/;
+$external_latex_labels{$key} = q|3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/perl6/;
+$external_latex_labels{$key} = q|5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/registros/;
+$external_latex_labels{$key} = q|3.2.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/asigArreglos/;
+$external_latex_labels{$key} = q|2.3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/clase/;
+$external_latex_labels{$key} = q|3.3.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/stringfunc/;
+$external_latex_labels{$key} = q|2.7.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/problBLAST/;
+$external_latex_labels{$key} = q|2.8.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/oneliners/;
+$external_latex_labels{$key} = q|6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/editores/;
+$external_latex_labels{$key} = q|2.1.2|; 
+$noresave{$key} = "$nosave";
+
 $key = q/fig:interfaz_graphviz/;
 $external_latex_labels{$key} = q|3.4.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/terminal/;
+$external_latex_labels{$key} = q|2.1.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sem2/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/aritfunc/;
+$external_latex_labels{$key} = q|2.7.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/bioperl/;
+$external_latex_labels{$key} = q|4.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graphviz/;
+$external_latex_labels{$key} = q|3.4.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/presentacion/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/variab_tipos/;
+$external_latex_labels{$key} = q|2.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/apoyo/;
+$external_latex_labels{$key} = q|10|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:clase/;
+$external_latex_labels{$key} = q|3.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:1lfu/;
+$external_latex_labels{$key} = q|2.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/problMatrSust/;
+$external_latex_labels{$key} = q|2.7.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/varEspecial/;
+$external_latex_labels{$key} = q|2.3.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/problEvalAlin/;
+$external_latex_labels{$key} = q|2.7.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/bibFunciones/;
+$external_latex_labels{$key} = q|2.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sem3/;
+$external_latex_labels{$key} = q|4|; 
 $noresave{$key} = "$nosave";
 
 1;
