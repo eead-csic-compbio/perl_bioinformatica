4a75324684e54333e7092b477400b68a82e9bcf3
eead-csic-compbio <compbio@eead.csic.es>
2017-08-07 11:47:35 +0200

    Added link to tutorial

--- images.pl
+++ images.pl
@@ -2,15 +2,11 @@
 # Associate images original text with physical files.
 
 
-$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{1lfu}{{center{{figure};LFS=12;FSF=1/;
+$key = q/Ngeq1;MSF=1.4;LFS=12/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="328" HEIGHT="387" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="\begin{figure}
-\begin{center}
-\includegraphics[width=0.8\textwidth]{1lfu}
-\end{center}
-\end{figure}">|; 
+ WIDTH="44" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$ N\geq1$">|; 
 
 $key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{DBIflux}{{center{{figure};LFS=12;FSF=1/;
 $cached_env_img{$key} = q|<IMG
@@ -22,13 +18,29 @@ $cached_env_img{$key} = q|<IMG
 \end{center}
 \end{figure}">|; 
 
-$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{BDoperaciones}{{center{{figure};LFS=12;FSF=1/;
+$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{arbolBin}{{center{{figure};LFS=12;FSF=1/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="547" HEIGHT="435" BORDER="0"
- SRC="|."$dir".q|img8.png"
+ WIDTH="547" HEIGHT="361" BORDER="0"
+ SRC="|."$dir".q|img5.png"
  ALT="\begin{figure}
 \begin{center}
-\includegraphics[width=0.8\textwidth]{BDoperaciones}
+\includegraphics[width=0.8\textwidth]{arbolBin}
+\end{center}
+\end{figure}">|; 
+
+$key = q/includegraphics[width=0.8textwidth]{subrutinas};LFS=12/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="390" HEIGHT="496" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\includegraphics[width=0.8\textwidth]{subrutinas}">|; 
+
+$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{clase}{{center{{figure};LFS=12;FSF=1/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="416" HEIGHT="295" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="\begin{figure}
+\begin{center}
+\includegraphics[width=0.8\textwidth]{clase}
 \end{center}
 \end{figure}">|; 
 
@@ -42,35 +54,23 @@ $cached_env_img{$key} = q|<IMG
 \end{center}
 \end{figure}">|; 
 
-$key = q/Ngeq1;MSF=1.4;LFS=12/;
+$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{BDoperaciones}{{center{{figure};LFS=12;FSF=1/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="44" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img1.png"
- ALT="$ N\geq1$">|; 
-
-$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{clase}{{center{{figure};LFS=12;FSF=1/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="416" HEIGHT="295" BORDER="0"
- SRC="|."$dir".q|img6.png"
+ WIDTH="547" HEIGHT="435" BORDER="0"
+ SRC="|."$dir".q|img8.png"
  ALT="\begin{figure}
 \begin{center}
-\includegraphics[width=0.8\textwidth]{clase}
+\includegraphics[width=0.8\textwidth]{BDoperaciones}
 \end{center}
 \end{figure}">|; 
 
-$key = q/includegraphics[width=0.8textwidth]{subrutinas};LFS=12/;
+$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{1lfu}{{center{{figure};LFS=12;FSF=1/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="390" HEIGHT="496" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="\includegraphics[width=0.8\textwidth]{subrutinas}">|; 
-
-$key = q/{figure}htmlimage{scale=1.5}center{includegraphics[width=0.8textwidth]{arbolBin}{{center{{figure};LFS=12;FSF=1/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="547" HEIGHT="361" BORDER="0"
- SRC="|."$dir".q|img5.png"
+ WIDTH="328" HEIGHT="387" BORDER="0"
+ SRC="|."$dir".q|img2.png"
  ALT="\begin{figure}
 \begin{center}
-\includegraphics[width=0.8\textwidth]{arbolBin}
+\includegraphics[width=0.8\textwidth]{1lfu}
 \end{center}
 \end{figure}">|; 
 
